031878eb309788d73abe805f40a5fb54a898ac21
Coder-neo-09 <neocoder09@gmail.com>
2025-09-17 20:36:53 +0530

    Add files via upload

---
+++ Python-Practice/3.Control-flow/loop-condition.py
@@ -0,0 +1,19 @@
+x=6
+
+# while x < 10:
+#     x+=1
+#     print(x)
+
+
+# for i in range(3,10,2):
+#     print(i)
+
+# for i in range(1,10):
+#     if i == 5:
+#         break
+#     print(i)
+
+for i in range(1,10):
+    if i == 6:
+        continue
+    print(i)
---
+++ Python-Practice/3.Control-flow/loops.py
@@ -0,0 +1,24 @@
+a=10
+
+fruits=["apple","banana","cherry"]
+fruit="cherry"
+# for fruit in fruits:
+#     print(fruit)
+
+# for i in range(5):
+#     print(i)
+
+
+x=5
+
+while x >=10:
+    x+=1  # x=x+1
+    print(x)
+else:
+    print("the condition fails")
+
+for i in range(2):
+    for j in range(3):
+        print(j,"j value is")
+    print(i,"I value is")
+
