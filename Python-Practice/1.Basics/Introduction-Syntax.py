d09b6f97d7ce8d6723ab832037df3a0f96236fc7
Coder-neo-09 <neocoder09@gmail.com>
2025-09-17 20:32:24 +0530

    Update Introduction-Syntax.py

---
+++ Python-Practice/1.Basics/Introduction-Syntax.py
@@ -0,0 +1,34 @@
+print("Hello world!")
+
+name=input("Enter your name:")
+print("you are",name,"Welcome")
+
+# If conditional statement
+if True:
+    print("Welcome to the world")
+    print("Welcome to Python")
+
+num=9
+
+if num > 0:
+    print("numbers is positive")
+if num < 0:
+    print("Number is negative")
+
+# Data types
+x=20
+y="Ramya"
+z=True
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+a="40"
+print(type(a))
+
+age=int(a)
+total = age + 20
+print(total)
+
+
