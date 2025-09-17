029c1644caec492e89408793fd99ca5c11dade43
Coder-neo-09 <neocoder09@gmail.com>
2025-09-17 20:33:59 +0530

    Update identity-member.py

---
+++ Python-Practice/2.Airthmetic-operations/identity-member.py
@@ -0,0 +1,17 @@
+# identity 
+
+a=[2,3,4]
+b=[2,3,4]
+c=a
+
+# print(a==b)
+
+# print(a is b)
+
+# print(a is c)
+
+# membership
+
+basket=["apple","banana","cherry","grapes"]
+
+print("banana" not  in basket)
