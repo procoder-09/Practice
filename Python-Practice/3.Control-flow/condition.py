d5996ce7c82676d707bde0a9c16db4bb8c5d9fbd
Coder-neo-09 <neocoder09@gmail.com>
2025-09-17 20:35:01 +0530

    Update condition.py

---
+++ Python-Practice/3.Control-flow/condition.py
@@ -0,0 +1,14 @@
+age=20
+
+# if(age<18):
+#     print("minor")
+# else:
+#     print("major")
+
+
+if(age<14):
+    print("child")
+elif(age<18):
+    print("minor")
+elif(age>18):
+    print("major")
