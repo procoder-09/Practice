42945edc82a70a512094fc6b26d7c71575284e27
Coder-neo-09 <neocoder09@gmail.com>
2025-09-17 20:39:37 +0530

    Update dicitionary.py

---
+++ Python-Practice/4.Data-strucures/dicitionary.py
@@ -0,0 +1,21 @@
+x={"name":"Amma","age":20 }  # key = value
+
+print(x)
+
+print(x.keys())
+
+print(x.values())
+
+x["name"]="alpha"
+
+print(x)
+
+x.pop("age")  # the total key and value both gets del
+
+print(x)
+
+
+
+y= {"a":{"a":2,"b":4 ,"c":7} , "b":{"c":6,"h":5,"f":8},"c": {"a":8}}
+
+print(y)
