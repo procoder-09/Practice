17baea9cc2135ee43885b4fe9e751400d08c706b
Coder-neo-09 <neocoder09@gmail.com>
2025-09-17 20:38:34 +0530

    Add files via upload

---
+++ Python-Practice/5.Functions/defining.py
@@ -0,0 +1,17 @@
+#  def function
+
+def add(x,y):
+    print(x+y)
+
+add(2,5)
+add(6,7)
+add(5,8)
+
+print("Multiplication function")
+def mul(x,y):
+    x+=1
+    y+=1
+    return x * y
+
+print(mul(2,3))
+print(mul(6,4))
