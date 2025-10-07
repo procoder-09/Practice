5e59853bcba0fa67e23928b65597445e1b428d74
Coder09 <neocoder09@gmail.com>
2025-10-07 10:38:57 +0530

    Update Numpy.py

---
+++ Python-Practice/10.Working-with-libraries/Numpy.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# 1D array
+arr1 = np.array([1, 2, 3, 4])
+print(arr1)  # [1 2 3 4]
+
+# 2D array
+arr2 = np.array([[1, 2], [3, 4]])
+print(arr2)
+
+print("-----------------------------------------------")
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+print(a + b)  # [5 7 9]
+print(a * b)  # [4 10 18]
+print(a ** 2) # [1 4 9]
+
+print("-----------------------------------------------")
+arr = np.array([1, 2, 3, 4, 5])
+
+print(np.mean(arr))  # 3.0
+print(np.sum(arr))   # 15
+print(np.max(arr))   # 5
+print(np.min(arr))   # 1
